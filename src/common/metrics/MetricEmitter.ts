3dd3c0b537c106d28058c8d95b1ef5db537290d1
jyma <897694714@qq.com>
2023-08-23 23:16:02 +0800

    Update MetricEmitter.ts
    disable uploads

--- src/common/metrics/MetricEmitter.ts
+++ src/common/metrics/MetricEmitter.ts
@@ -11,8 +11,14 @@ export interface Emitter {
 export default class MetricEmitter {
   private static instance: Emitter | undefined;
   public static Instance (): Emitter {
+    
     if (!MetricEmitter.instance) { 
+        
+        if (config.getOrDefault('metrics.enabled', true)) {
         MetricEmitter.instance = new NoopMetricEmitter();
+      } else {
+        MetricEmitter.instance = new NoopMetricEmitter();
+      }
     }
 
     return MetricEmitter.instance;
